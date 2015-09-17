1b64162ceb4a2c9094ade551b80adbc1d3ab53e6
NewClass10 <leksekon@i.ua>
2015-09-17 21:19:52 +0300

    hello, world commit

---
+++ hillel/src/MyFirstClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by NewClass10 on 17.09.2015.
+ */
+public class MyFirstClass {
+  public static void main(String[] args){
+      String helloMassage = "Hello,world";
+      System.out.println("helloMassage");
+
+      int vladBirthYear = 1994;
+
+      int vladAge = 21;
+      System.out.println("I'm " + vladAge);
+  }
+}
+
