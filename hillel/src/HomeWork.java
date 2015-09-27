e8861de16fc086972d63840e271586fa95025411
Влад <leksekon@i.ua>
2015-09-28 01:34:50 +0300

    Homework1.2

--- hillel/src/HomeWork.java
+++ hillel/src/HomeWork.java
@@ -21,16 +21,15 @@ public class HomeWork {
 
         int number = scanner.nextInt();
 
-        if (number%2 ==0) {
+        if (number % 2 == 0) {
             System.out.println("Your number is even");
-        }
-        else {
+        } else {
             System.out.println("Your number is odd ");
 
         }
     }
 
-    private static void closeNumber(){
+    private static void closeNumber() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your first number: ");
@@ -40,74 +39,55 @@ public class HomeWork {
         int number2 = scanner.nextInt();
 
         int constantNumber = 10;
-        int res1 = constantNumber - number1;
+        int result1 = constantNumber - number1;
 
-        if (res1>0){
-            res1 = res1;
-        }
-
-        else{
-            res1 = res1 *-1;
-        }
-
-
-
-        int res2 = constantNumber - number2;
-
-        if (res2>0){
-            res2 = res2;
-        }
-
-        else{
-            res2= res2 *-1;
+        if (result1 > 0) {
+            result1 = result1;
+        } else {
+            result1 = Math.abs(result1);
         }
 
 
+        int result2 = constantNumber - number2;
 
-        if (res1<res2){
+        if (result2 > 0) {
+            result2 = result2;
+        } else {
+            result2 = Math.abs(result2);
+        }
+
+
+        if (result1 < result2) {
             System.out.println("First number is the nearest to 10 ");
-        }
-        else if(res1 == res2){
+        } else if (result1 == result2) {
             System.out.println("First and second numbers are near 10");
-        }
-        else{
+        } else {
             System.out.println("Second number is the nearest to 10");
         }
 
     }
 
-    private static void rectangle(){
+    private static void rectangle() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter perimeter of your rectangle : ");
-        int number1 = scanner.nextInt();
+        System.out.print("Enter perimeter of your rectangle(in cm) : ");
+        double perimetr = scanner.nextInt();
 
-        System.out.print("Enter area of your rectangle ");
-        int number2 = scanner.nextInt();
+        System.out.print("Enter area of your rectangle(in cm) : ");
+        double area = scanner.nextInt();
 
-        int summ = number1/2;
-
-
-        int b;
+        double summ = perimetr / 2;
 
 
-        for (b = 1;b<summ;b++){
-            if(b*b == summ*b - number2) {
-
-                System.out.println("Rectangle height: " + b);
-                System.out.println("Rectangle lenght: " + (summ - b));
-                break;
-            }
-            else{
-                System.out.println("rectangle isn't valid ");
-                break;
-            }
-
-
-
-
+        double discriminant = (summ * summ) - (4 * area);
+        if (discriminant > 0) {
+            double lenth = ((summ + Math.sqrt(discriminant)) / 2);
+            double height = ((summ - Math.sqrt(discriminant)) / 2);
+            System.out.println("Rectange lehth is " + lenth + " centimeters");
+            System.out.println("Rectange height is " + height + " centimeters");
+        } else {
+            System.out.println("Rectangle doesnt exist ");
         }
-
 
 
     }
