2f372c3d818582df3050a17906cbb70885c4dc09
Влад <leksekon@i.ua>
2015-10-15 12:11:59 +0300

    Homework5.1

--- hillel/src/homeWork/RectangleTask.java
+++ hillel/src/homeWork/RectangleTask.java
@@ -5,15 +5,46 @@ package homeWork;
  */
 public class RectangleTask {
 
-    int perimetr;
-    int area;
+  private  double perimetr;
+  private  double area;
 
-    public RectangleTask(int perimetr,int area){
+    public RectangleTask(double perimetr,double area){
         this.perimetr = perimetr;
         this.area = area;
 
+
     }
-    double d = (perimetr/2) * (4*area);
-    double height = ((perimetr/2) - Math.sqrt(d))/2;
-    double width = ((perimetr/2) + Math.sqrt(d))/2;
+    public boolean hasSolution(){
+       return  ((perimetr/2) * (4*area))>0?true:false ;
+
+    }
+
+    public Rectangle getRectangle(){
+
+        double d = ((Math.pow(perimetr/2,2)) - (4*area));
+        double height = ((perimetr/2) - Math.sqrt(d))/2;
+        double width = ((perimetr/2) + Math.sqrt(d))/2;
+
+        Rectangle rectangle = new Rectangle(height,width);
+
+        return rectangle;
+    }
+}
+
+
+
+ class Rectangle {
+    public double height;
+    public  double width;
+
+    public Rectangle(double height,double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public static void main(String[] args) {
+        RectangleTask rect1 = new RectangleTask(22,30);
+
+        System.out.println(rect1.getRectangle());
+    }
 }
