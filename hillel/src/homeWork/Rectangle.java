a35404097cc5df78f6010185309fb5b94ae4284c
Влад <leksekon@i.ua>
2015-10-15 14:11:16 +0300

    Homework5.2

--- hillel/src/homeWork/Rectangle.java
+++ hillel/src/homeWork/Rectangle.java
@@ -1,10 +1,36 @@
 package homeWork;
 
 /**
- * Created by Влад on 09.10.2015.
+ * Created by Влад on 15.10.2015.
  */
 public class Rectangle {
+    public double height;
+    public double width;
+    public String toString()
+    {
+        return "height is " + height + " and width is " + width;
+    }
+
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+
     public static void main(String[] args) {
+
+
+
+        RectangleTask rect1 = new RectangleTask(22, 30);
+        RectangleTask rect2 = new RectangleTask(16, 7);
+        RectangleTask rect3 = new RectangleTask(14, 12);
+        RectangleTask rect4 = new RectangleTask(3, 5);
+        System.out.println(rect1.getRectangle());
+        System.out.println(rect2.getRectangle());
+        System.out.println(rect3.getRectangle());
+        System.out.println(rect4.getRectangle());
+
 
     }
 }
--- hillel/src/homeWork/RectangleTask.java
+++ hillel/src/homeWork/RectangleTask.java
@@ -8,43 +8,43 @@ public class RectangleTask {
   private  double perimetr;
   private  double area;
 
+
     public RectangleTask(double perimetr,double area){
         this.perimetr = perimetr;
         this.area = area;
 
 
-    }
-    public boolean hasSolution(){
-       return  ((perimetr/2) * (4*area))>0?true:false ;
+
 
     }
+    public boolean hasSolution(){
+       return  ((perimetr/2) * (4*area))>0;
+
+    }
+
 
     public Rectangle getRectangle(){
 
-        double d = ((Math.pow(perimetr/2,2)) - (4*area));
-        double height = ((perimetr/2) - Math.sqrt(d))/2;
-        double width = ((perimetr/2) + Math.sqrt(d))/2;
+        if (hasSolution() == true) {
 
-        Rectangle rectangle = new Rectangle(height,width);
 
-        return rectangle;
+            double d = ((Math.pow(perimetr / 2, 2)) - (4 * area));
+            double height = ((perimetr / 2) - Math.sqrt(d)) / 2;
+            double width = ((perimetr / 2) + Math.sqrt(d)) / 2;
+
+
+
+            Rectangle rectangle = new Rectangle(height, width);
+
+            return rectangle;
+
+        }
+            return null;
+
     }
 }
 
 
 
- class Rectangle {
-    public double height;
-    public  double width;
 
-    public Rectangle(double height,double width) {
-        this.height = height;
-        this.width = width;
-    }
 
-    public static void main(String[] args) {
-        RectangleTask rect1 = new RectangleTask(22,30);
-
-        System.out.println(rect1.getRectangle());
-    }
-}
